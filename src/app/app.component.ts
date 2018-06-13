5dcfd0a6f2dd941de41e8679456577d541373748
anupama8890 <AnupamaS@SycaSol.com>
2018-06-13 10:55:32 +0530

    Bulb Assignment Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+    arrs=[0,0,0,0];
+    bulbs=['../assets/images/off.png','../assets/images/off.png','../assets/images/off.png','../assets/images/off.png'];
+    btns=['ON','ON','ON','ON'];
+    status:boolean=false;
+    
+    currentValue;
+    changeBulb(currentValue) {
+      if(this.status==false) {
+       // console.log(this.status);
+        this.status=true;
+        this.bulbs[currentValue]="../assets/images/on.png";
+        this.btns[currentValue]="OFF";
+
+      }
+
+      else {
+        //console.log(this.status);
+        this.status=false;
+        this.bulbs[currentValue]="../assets/images/off.png";
+        this.btns[currentValue]="ON";
+
+      }
+    }
 }
+
+
